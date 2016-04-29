e9a69f232590becd999d46a7a7ff5c50709f3ce0
OlehKulykov <info@resident.name>
2016-04-29 14:59:05 +0200

    Update color constant colores

--- OKAlertController/Model/Misc/MyColors.swift
+++ OKAlertController/Model/Misc/MyColors.swift
@@ -24,17 +24,35 @@
 import UIKit
 
 
+/**
+Named color constants commonly used within the application.
+*/
 public struct MyColors {
 
-	// #4A4D55
+	/**
+	Alert controller title and message text color.
+	Hex value: #4A4D55
+	*/
 	static var AlertTextColor = UIColor(red: 74.0 / 255, green: 77.0 / 255, blue: 85.0 / 255, alpha: 1)
 
-	// #979797
+
+	/**
+	Alert controller window border color.
+	Hex value: #979797
+	*/
 	static var AlertBorderColor = UIColor(red: 151.0 / 255, green: 151.0 / 255, blue: 151.0 / 255.0, alpha: 1)
 
-	// #6D9EE1
+
+	/**
+	Alert controller normat buttons text color.
+	Hex value: #6D9EE1
+	*/
 	static var AlertNormalButtonTextColor = UIColor(red: 109.0 / 255, green: 158.0 / 255, blue: 225.0 / 255, alpha: 1)
 
-	// #D3D3D3
+
+	/**
+	Alert controller dismiss or cancel text color.
+	Hex value: #D3D3D3
+	*/
 	static var AlertDismissButtonTextColor = UIColor(red: 211.0 / 255, green: 211.0 / 255, blue: 211.0 / 255, alpha: 1)
 }
