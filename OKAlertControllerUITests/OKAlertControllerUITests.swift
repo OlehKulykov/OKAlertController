32573c8846ac7519456e2870ac53d4301504e9a3
OlehKulykov <info@resident.name>
2016-05-04 22:56:38 +0200

    Disable Travis UI tests

--- OKAlertControllerUITests/OKAlertControllerUITests.swift
+++ OKAlertControllerUITests/OKAlertControllerUITests.swift
@@ -39,12 +39,7 @@ class OKAlertControllerUITests: XCTestCase {
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
+
     func testShowHide() {
 		XCUIDevice.sharedDevice().orientation = .Portrait
 		
